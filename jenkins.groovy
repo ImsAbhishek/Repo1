7241a4734c9d5e2b0d114874131ebd6e2d30c589
ImsAbhishek <51257437+ImsAbhishek@users.noreply.github.com>
2022-01-22 15:46:46 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,10 +1,20 @@
 pipeline { 
    agent any
+   environment {
+      secret_file = credentials{'jenkins.pem'}
+    }
 stages {
-  stage("Build") {
+  stage("Ec2-connection") {
   steps {
 script {
 sh 'echo "Hey Abhishek"'
+sh 'shh -i $secret_file ec2-user@http://34.228.12.253'
+}
+}}
+  stage("status") {
+  steps {
+script {
+sh 'echo "success"'
 }
 }}
 }
