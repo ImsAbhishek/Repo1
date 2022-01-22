0121c4db99714bc3086087f4f3111a0ace894c13
ImsAbhishek <51257437+ImsAbhishek@users.noreply.github.com>
2022-01-22 21:15:47 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,23 +2,48 @@ pipeline {
    agent any
   
 stages {
-  stage("Ec2-connection") {
+stage("SCM checkout") {
   steps {
-     withCredentials([sshUserPrivateKey(credentialsId: 'server',   keyFileVariable: 'key', passphraseVariable: 'passphrase', usernameVariable: 'username')])
-     
-script {
-sh 'echo "Hey Abhishek"'
-   sh 'echo "credentialsId: $server"'
-sh 'echo "keyFileVariable: $key"'
-sh 'echo "passphraseVariable: $passphrase"'
-sh 'echo "usernameVariable: $username"'
-
-}
-}}
-  stage("status") {
-  steps {
+sh 'SCM checkout"'
 script {
 sh 'echo "success"'
+}
+}}
+  stage("Maven Build") {
+  steps {
+script {
+sh 'echo "Maven Build"'
+}
+}}
+   
+stage("sonarqube code anlysis") {
+  steps {
+script {
+sh 'echo "sonarqube code anlysis"'
+}
+}}
+stage("docker build") {
+  steps {
+script {
+sh 'echo "docker build"'
+}
+}}
+stage("Nexus image push") {
+  steps {
+script {
+sh 'echo "Nexus image push"'
+}
+}}
+stage("Docker deploy") {
+  steps {
+script {
+sh 'echo "Docker deploy"'
+}
+}}
+  stage("email notification") {
+  steps {
+script {
+sh 'echo "email notification"'
 }
 }}
 }
