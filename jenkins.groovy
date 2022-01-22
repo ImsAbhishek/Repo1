ef28db6e1cc82966edfb48b5882a01bf0481f567
ImsAbhishek <51257437+ImsAbhishek@users.noreply.github.com>
2022-01-22 18:57:41 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,14 +1,18 @@
 pipeline { 
    agent any
-   environment {
-      secret_file = credentials{'jenkins.pem'}
-    }
+  
 stages {
   stage("Ec2-connection") {
   steps {
+     withCredentials([sshUserPrivateKey(credentialsId: 'server',   keyFileVariable: 'key', passphraseVariable: 'passphrase', usernameVariable: 'username')])
+     
 script {
 sh 'echo "Hey Abhishek"'
-sh 'shh -i $secret_file ec2-user@http://34.228.12.253'
+   sh 'echo "credentialsId: $server"'
+sh 'echo "keyFileVariable: $key"'
+sh 'echo "passphraseVariable: $passphrase"'
+sh 'echo "usernameVariable: $username"'
+
 }
 }}
   stage("status") {
