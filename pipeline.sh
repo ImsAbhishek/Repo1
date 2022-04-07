550aac6065e8373ef8f7fc88cdc884a03e04e8fe
ayush.ganatra <ayush.ganatra@myglamm.com>
2022-04-07 16:46:54 +0000

    pipeline.sh edited online with Bitbucket

--- pipeline.sh
+++ pipeline.sh
@@ -13,5 +13,12 @@ curl -X POST -is -u  $client_id:$secret \
     "ref_type": "branch",
     "type": "pipeline_ref_target",
     "ref_name": "master"
-  }
+  },
+
+  "variables": [
+      {
+        "key": "file",
+        "value": "testFile"
+      }
+    ]
 }'
