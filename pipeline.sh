5b47eeae057f759479492c68f13a98474fc9c89b
abhishek.singh <abhishek.singh@myglamm.com>
2022-04-29 06:09:49 +0000

    pipeline.sh edited online with Bitbucket

--- pipeline.sh
+++ pipeline.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-branch=dev
+branch=mygalmm
 GIT=`which git`
 # REPO_DIR=/
 # cd ${REPO_DIR}
-wpcomvip/babychakra.
 ${GIT} init
-ss=$(${GIT} ls-remote --heads https://wpcomvip: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@github.com/wpcomvip/babychakra.git $branch | wc -l)
+ss=$(${GIT} ls-remote --heads https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch | wc -l)
 if [[ "$ss" == 0 ]];
 then
  echo "branch does't exist"
@@ -16,5 +15,5 @@ else
 fi
 ${GIT} add --all .
 ${GIT} commit -m "Test commit"
-${GIT} push https://wpcomvip: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@github.com/wpcomvip/babychakra.git $branch
+${GIT} push https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch
     
