831d1b2c2344e30282e7e734c4246e579b7f5979
vishal0915 <15singhabhishekrajput@gmail.com>
2022-05-02 01:16:15 +0530

    Test commit

---
+++ ss.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+branch=qq
+GIT=`which git`
+# REPO_DIR=/
+# cd ${REPO_DIR}
+${GIT} init
+ss=$(${GIT} ls-remote --heads https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch | wc -l)
+echo "$ss"
+if [[ "$ss" == 0 ]];
+then
+ echo "branch does't exist"
+ ${GIT} checkout -b $branch
+else
+    echo "branch exist"
+   ${GIT} fetch --all
+   ${GIT} checkout $branch
+fi
+${GIT} add --all .
+${GIT} commit -m "Test commit"
+${GIT} push https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch
