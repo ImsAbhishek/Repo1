c5744fe43cec9c90770833c70af2ec29bffa2d5e
vishal0915 <15singhabhishekrajput@gmail.com>
2022-05-02 08:50:40 +0530

    Test commit

--- ss.sh
+++ ss.sh
@@ -4,7 +4,7 @@ GIT=`which git`
 # REPO_DIR=/
 # cd ${REPO_DIR}
 ${GIT} init
-ss=$(${GIT} ls-remote --heads https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch | wc -l)
+ss=$(${GIT} ls-remote --heads https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch | wc -l)
 echo "$ss"
 if [[ "$ss" == 0 ]];
 then
@@ -12,12 +12,12 @@ then
  ${GIT} checkout -b $branch
 else
     echo "branch exist"
-   ${GIT} fetch --all
-  # ${GIT} push --delete https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch
-   ${GIT} checkout  $branch
+   ${GIT} push --delete https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch
+   ${GIT} fetch -p
+   ${GIT} checkout -b  $branch
 fi
  ${GIT} add --all .
 ab=`${GIT} status`
 echo $ab
 ${GIT} commit -m "Test commit"
-${GIT} push https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch
+${GIT} push --force https://ImsAbhishek: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@github.com/ImsAbhishek/Repo1.git $branch
